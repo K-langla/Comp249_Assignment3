0552635861d52e5f17e2c9b2ff38b0fe48baec2a
K_Langlais <kalanglais@gmail.com>
2018-02-26 18:36:04 -0500

    testing files

--- Comp249Assignment3/src/base/BibCreator.java
+++ Comp249Assignment3/src/base/BibCreator.java
@@ -1,9 +1,9 @@
+/*------------------------------------------------------------------------------
+    Assignment 3
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    Written by: Jason Brennan 27793928
+                Kelly Langlais 26919855
+------------------------------------------------------------------------------*/
 package base;
 
 /**
