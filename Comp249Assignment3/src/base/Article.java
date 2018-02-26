b331ffee82656e86b24fed295884e96f9b2452b4
K_Langlais <kalanglais@gmail.com>
2018-02-26 18:36:42 -0500

    testing files

--- Comp249Assignment3/src/base/Article.java
+++ Comp249Assignment3/src/base/Article.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*------------------------------------------------------------------------------
+    Assignment 2
+    Part 2
+    Written by: Jason Brennan 27793928
+                Kelly Langlais 26919855
+------------------------------------------------------------------------------*/
 package base;
 
 /**
@@ -13,4 +14,21 @@ public class Article {
     
     String author,title,journal,volume,pages, DOIaddress,month;
     final static String DOI = "https://doi.org/";
+    int year, number;
+    
+    public Article(String author,String title,String journal,String volume,String pages,String DOIaddress,String month,int year, int number ){
+        
+        this.author = author;
+        this.title= title;
+        this.journal = journal;
+        this.volume = volume;
+        this.pages = pages;
+        this.DOIaddress= DOIaddress;
+        this.month= month;
+        this.year=year;
+        this.number=number;
+                
+    }
+    
+    
 }
