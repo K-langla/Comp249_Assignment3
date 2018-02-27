dbee1c8775cbea29b6629979feb7e339888ac010
K_Langlais <kalanglais@gmail.com>
2018-02-26 20:52:05 -0500

    added Article methods

--- Comp249Assignment3/src/base/Article.java
+++ Comp249Assignment3/src/base/Article.java
@@ -12,9 +12,10 @@ package base;
  */
 public class Article {
     
-    String author,title,journal,volume,pages, DOIaddress,month;
-    final static String DOI = "https://doi.org/";
-    int year, number;
+    private String author,title,journal,volume,pages, DOIaddress,month;
+    final static String DOI = "https://doi.org/", DELIM ="[ and ]";
+    private int year, number;
+    private String[] authors;
     
     public Article(String author,String title,String journal,String volume,String pages,String DOIaddress,String month,int year, int number ){
         
@@ -27,8 +28,93 @@ public class Article {
         this.month= month;
         this.year=year;
         this.number=number;
+        authors =author.split(DELIM);
                 
     }
     
+    public String IEEEtoString(){
+        
+        return IEEEformat(getAuthors()) +", "+getTitle()+", "+getJournal()+", vol. "+getVolume()+", "+getNumber()+", "+getPages()+", "+
+                getMonth()+", "+getYear()+".";
+        
+    }
+    
+    public String ACMtoString(){
+        
+        return ACMformat(getAuthors())+getYear()+". "+getTitle()+". "+getJournal+". "+getVolume()+", "+
+                getNumber()+". ("+getYear()+"), "+getPages()+". "+DOI+getDOIaddress()+".";
+        
+    }
+    
+    public String NJtoString(){
+        
+        return NJformat(getAuthors())+". "+getTitle()+". "+getJournal+". "+ getVolume()+", "+getPages+"("+getYear()+").";
+    }
+    
+    public String IEEEformat(String[] authors){
+        
+        StringBuilder format = new StringBuilder();
+        
+        for(int i=0; i<authors.length-1;i++){
+            
+            format.append(authors[i]);
+            
+            if (i<authors.length-1)
+                format.append(", ");
+        }
+        return format.toString();
+    }
+    
+    public String ACMformat(String[] authors){
+        
+        return authors[0]+" et al. ";
+    }
+    
+    public String NJformat(String[] authors){
+        
+        StringBuilder format = new StringBuilder();
+        
+        for(int i=0; i<authors.length-1;i++){
+            
+            format.append(authors[i]);
+            
+            if (i<authors.length-1)
+                format.append(" & ");
+        }
+        return format.toString();
+        
+    }
+    
+    public String getAuthor(){
+        return author;
+    }
+    public String getTitle(){
+        return title;
+    }    
+    public String getJournal(){
+        return journal;
+    }    
+    public String getVolume(){
+        return volume;
+    }
+    public String getPages(){
+        return pages;
+    }
+    public String getDOIaddress(){
+        return DOIaddress;
+    }
+    public String getMonth(){
+        return month;
+    }
+    public int getYear(){
+        return year;
+    }
+    public int getNumber(){
+        return number;
+    }   
+    
+    public String[] getAuthors(){
+        return authors;
+    }
     
 }
